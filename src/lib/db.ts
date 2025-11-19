e9d8e4ce301ba17e0b1727f39e4bdbd13c35a65b
HAKAN TASKIN <hakan.taskin@example.com>
2025-11-19 23:55:37 +0000

    DBURL changes for vercel

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,4 +3,6 @@ import { Pool } from "pg";
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  // if your provider requires SSL:
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
